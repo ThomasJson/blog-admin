118748d8cfaf7e1a54496ebff19327585d98c774
Thomas Json <joogletom8@gmail.com>
2022-09-21 14:50:31 +0200

    SCREENS ++

--- src/screens/ThemeScreen.jsx
+++ src/screens/ThemeScreen.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 const ThemeScreen = () => {
-    return (
-        <>
-             <h1>Liste des thèmes</h1>
-        </>
-    );
+    
+  const [themes, setThemes] = useState([]);
+
+  // Nous utilisons un useEffect qui ne s’exécute qu’une fois
+  // (au chargement du composant)
+
+  useEffect(() => {
+    fetch("http://blog-api.loc/theme")
+      .then((resp) => resp.json())
+      // Une fois les données récupérées,
+      // nous mettons à jour le state (avec set...)
+
+      // Pour afficher les mots-clés par ordre alphabétique,
+      // nous modifions le useEffect en ajoutant un
+      // sort sur le json reçu de l’api rest
+      .then((json) => {
+        json = json.sort((a, b) => {
+          return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1;
+        });
+        setThemes(json);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Liste des Thèmes</h1>
+      {/* nous utilisons map sur le state tags afin de créer les lignes 
+      dans le tbody du tableau */}
+      <table>
+        <tbody>
+          {themes.map((theme) => {
+            return (
+              <tr key={theme.Id_theme}>
+                <td>{theme.title}</td>
+                <td></td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default ThemeScreen;
