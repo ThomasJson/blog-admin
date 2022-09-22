f39d7597b042464e86506f09be03ae1fd7fc978d
Thomas Json <joogletom8@gmail.com>
2022-09-22 10:27:38 +0200

    E40 en cours

--- src/screens/TagDetailScreen.jsx
+++ src/screens/TagDetailScreen.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+// Récupérer l’id de la route
 
 const TagDetailScreen = () => {
+    const {id} = useParams();
+
     return (
         <>
-            <h1>Détail du mot-clé</h1>
+            <h1>Détail du mot-clé : {id}</h1>
         </>
     );
 };
